366ffd666564bcd797c02aa9ac64706e0c395e3c
Ronnie Serr <ronnie@alphabethouse.com>
2024-05-23 07:45:43 -0700

    Prepare for multiple hosts

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
+const APP_PORT = process.env.APP_PORT || 3000;
+const HOST_NAME = process.env.HOST_NAME;
+const GIT_BRANCH = process.env.GIT_BRANCH;
+
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World ' + HOST_NAME  + ' | Branch: ' + GIT_BRANCH + '!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(APP_PORT, function () {
+  console.log('Example app listening on port ' + APP_PORT + ' | Host: ' + HOST_NAME  + ' | Branch: ' + GIT_BRANCH + '!');
 });
